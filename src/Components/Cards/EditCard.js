8594633baed1cfd89dc988fe123f0455b3368df4
Brett <brett_neighbors@yahoo.com>
2024-02-06 20:51:14 -0700

    reverting back to commit 51bedbc in order to have AddCard work properly. Need to update EditCard to get functionality to work.

--- src/Components/Cards/EditCard.js
+++ src/Components/Cards/EditCard.js
@@ -4,50 +4,30 @@ import { readDeck } from "../../utils/api"
 import Form from "./Form";
 
 export default function EditCard() {
-  const {deckId, cardId } = useParams();
-  const [deck, setDeck] = useState({cards: []});
-  const [card, setCard] = useState(null);
+    const {deckId, cardId } = useParams();
+    const [deck, setDeck] = useState({cards: []});
 
-  useEffect(() => {
-    async function loadCard() {
-      const response = await readDeck(deckId);
-      if (response.ok) {
-        const deck = await response.json();
-        const card = deck.cards.find((c) => c.id === parseInt(cardId));
-        if (card) {
-          setCard(card);
-          setDeck(deck);
-        } else {
-          setCard(null);
-          setDeck({cards: []});
-        }
-      } else {
-        setCard(null);
-        setDeck({cards: []});
-      }
-    }
-    loadCard();
-    // eslint-disable-next-line 
-  }, [deckId, cardId]);
+    useEffect(() => {
+        readDeck(deckId).then(setDeck)
+        
+      // eslint-disable-next-line 
+      }, [deckId, cardId]);
 
-  return (
-    <>
-      {!card && <p>Error: Card not found</p>}
-      {card && (
-        <>
-          <div className="breacrumbs">
-            <nav aria-label="breadcrumb">
-              <ol className="breadcrumb">
-                <li className="breadcrumb-item"><Link to="/"><span className="oi oi-home"></span> Home</Link></li>
-                <li className="breadcrumb-item"><Link to={`/decks/${deck.id}`}>{deck.name}</Link></li>
-                <li className="breadcrumb-item active" aria-current="page">Edit Card {cardId}</li>
-              </ol>
-            </nav>
-          </div>
-          <h1>Edit Card</h1>
-          <Form card={card} submitting="edit card" deck={deck}/>
-        </>
-      )}
-    </>
-  )
+
+return (
+<>
+    <div className="breacrumbs">
+    <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+            <li className="breadcrumb-item"><Link to="/"><span className="oi oi-home"></span> Home</Link></li>
+            <li className="breadcrumb-item"><Link to={`/decks/${deck.id}`}>{deck.name}</Link></li>
+            <li className="breadcrumb-item active" aria-current="page">Edit Card {cardId}</li>
+        </ol>
+    </nav>
+    </div>
+    <h1>Edit Card</h1>
+    <Form submitting="edit card"/>
+
+</>
+)
 }
